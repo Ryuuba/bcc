a5ab2aeb8e2924e8b3b99332b7fab8c331f96e04
Adán G. Medrano-Chávez <geo@Adans-MacBook-Pro.local>
2017-01-09 13:31:39 -0600

    little changes done

--- simple.cpp
+++ simple.cpp
@@ -2,7 +2,6 @@
 
 #include <iostream>
 #include <stdio.h>
-#include <conio.h>
 #include <stdlib.h>
 #include <math.h>
 
@@ -14,8 +13,8 @@ struct Evt {
 	struct Evt *sig;
 	};
 
-double Tsim=0.0;     	//Tiempo de simulación (reloj)
-struct Evt *head=NULL;   //Apunta siempre al primer evento de la LEF o a NULL si ésta está vacía
+double Tsim=0.0;     	//Tiempo de simulaciï¿½n (reloj)
+struct Evt *head=NULL;   //Apunta siempre al primer evento de la LEF o a NULL si ï¿½sta estï¿½ vacï¿½a
 
 
 double uniforme(){
@@ -30,7 +29,7 @@ double expneg(double media){
 }
 
 
-double Erlang_B(unsigned int s,double a){//s: servidores, a: tráfico ofrecido
+double Erlang_B(unsigned int s,double a){//s: servidores, a: trï¿½fico ofrecido
 	double IB=1.0;
 	for (int j=1;j<=s;j++)	IB=1.0+j*IB/a;
     return (1.0/IB);
@@ -40,26 +39,26 @@ double Erlang_B(unsigned int s,double a){//s: servidores, a: tráfico ofrecido
 enum EvtTyp evento_actual(){
 	struct Evt *p=head;                      //Apuntador auxiliar. Sirve para liberar la memoria del primer evento de la LEF
 	enum EvtTyp evt_inminente=head->tipo;    //Evento a procesar en main.
-	Tsim=head->t_evento;                     //Actualiza el tiempo de simulación.		           
-    head=head->sig;                          //Ahora head apunta al siguiente evento de la LEF o a Null si la lista e quedó vacía
+	Tsim=head->t_evento;                     //Actualiza el tiempo de simulaciï¿½n.		           
+    head=head->sig;                          //Ahora head apunta al siguiente evento de la LEF o a Null si la lista e quedï¿½ vacï¿½a
 	free(p);                                 //Libera memoria cupada por el primer evento de la LEF        
-	return evt_inminente;                    //regresa el tipo de evento que se procesará
+	return evt_inminente;                    //regresa el tipo de evento que se procesarï¿½
 }
 
 
 void programa_evento(enum EvtTyp type, double epoch){
 	struct Evt *p; //Apuntador auxiliar para ir recorriendo la LEF
-	bool insertado=0;  // insertado=0 indica que aún no se ha insertado el nuevo evento en la LEF
+	bool insertado=0;  // insertado=0 indica que aï¿½n no se ha insertado el nuevo evento en la LEF
    
     struct Evt *newevt=(struct Evt *) malloc(sizeof(struct Evt));//Asigna memoria para el evento nuevo
 	newevt->tipo=type; newevt->t_evento=epoch; //Datos del evento a programar
 	
-	newevt->sig=head; //"newevt->sig" apunta a NULL si la LEF está vacía o al primer evento de la LEF
+	newevt->sig=head; //"newevt->sig" apunta a NULL si la LEF estï¿½ vacï¿½a o al primer evento de la LEF
 	while (insertado==0){
 		if(newevt->sig==NULL || (newevt->t_evento < (newevt->sig)->t_evento) ){
 			if (newevt->sig==head) head=newevt; //nuevo se inserta al inicio de la LEF
 			else p->sig=newevt;  //El evento nuevo se inserta entre dos eventos de la LEF o al final de la LEF
-			insertado=1;      //Indica que ya se insertó el evento nuevo en la LEF
+			insertado=1;      //Indica que ya se insertï¿½ el evento nuevo en la LEF
 		}
 		else {
 			p=newevt->sig; //El apuntador auxiliar "p" apunta al evento al que actualmente apunta "nuevo->sig"
@@ -77,7 +76,7 @@ int main(){
 	unsigned long arribos=0, bloqueados=0, N=1000000, j=0;//N=arribos a simular, j=estado del sistema	
 	double mu=1/180.0;   	//Ts=1/mu: tiempo medio de servicio, en segundos/llamada
 	int s=10; 				//Servidores
-	double a=5;				//Tráfico ofrecido	
+	double a=5;				//Trï¿½fico ofrecido	
 	
 	//std::cout<<"\nNumero de arribos a simular: "<<std::endl;
 	//std::cin>>N;	
@@ -101,8 +100,8 @@ int main(){
     }//end while
 
 	//Reporte de resultados
-	double Pb=(float)bloqueados/arribos;   //Congestión en llamada (simulador)
-	double Eb=Erlang_B(s,a);            //Congestión en tiempo analitico (Erlang B)
+	double Pb=(float)bloqueados/arribos;   //Congestiï¿½n en llamada (simulador)
+	double Eb=Erlang_B(s,a);            //Congestiï¿½n en tiempo analitico (Erlang B)
 
 	std::cout<<"\n\n------------------ Resultados (Sistema BCC)----------------"<<std::endl;
 	std::cout<<"\ns= "<<s<<", a="<<a<<std::endl;
